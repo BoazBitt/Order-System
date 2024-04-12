637384d4889b66c516902f6c9e86a26af12b669d
Boaz Bitton <boaz2119@gmail.com>
2024-04-13 00:02:04 +0300

    updated Settings

--- Server/orderHub/settings.py
+++ Server/orderHub/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['localhost','Boaz21115.pythonanywhere.com']#
 
 CORS_ALLOWED_ORIGINS = [
     'https://order-system-rv1i.onrender.com',
-    'http://localhost:3000/'
+    'http://localhost:3000'
 
 ]
 # Application definition
