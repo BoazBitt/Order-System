04813500b7e9424d6279ef68a3c62893ed2b1cdf
Boaz Bitton <boaz2119@gmail.com>
2024-04-13 00:16:16 +0300

    updated API call

--- Client/src/utilis/actions/APIEndpoint.ts
+++ Client/src/utilis/actions/APIEndpoint.ts
@@ -1,5 +1,5 @@
 // export const PATH_SERVER = "http://127.0.0.1:8000";
-export const PATH_SERVER = "Boaz21115.pythonanywhere.com";
+export const PATH_SERVER = "http://Boaz21115.pythonanywhere.com";
 export const HEADERS = {
   "Content-Type": "application/json",
 };
