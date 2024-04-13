c17c90b1002db7706c53fe98fbe565d3191d517c
Boaz Bitton <boaz2119@gmail.com>
2024-04-13 20:28:54 +0300

    update vite.config

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,10 +1,8 @@
-import vitePluginRequire from "vite-plugin-require";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react(), vitePluginRequire()],
+  plugins: [react()],
 });
