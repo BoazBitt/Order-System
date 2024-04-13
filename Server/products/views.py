e14181a7f2b78a3d96e0d097dfc8dcc0bdddded3
Boaz Bitton <boaz2119@gmail.com>
2024-04-13 03:35:30 +0300

    update pagination

--- Server/products/views.py
+++ Server/products/views.py
@@ -5,9 +5,9 @@ from .serializer import ProductSerializer
 from rest_framework.pagination import PageNumberPagination
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10  
+    page_size = 3  
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 9
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
