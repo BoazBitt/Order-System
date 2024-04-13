9acf29beb8f6ea8a37593521b4edb8c7ea01ad98
Boaz Bitton <boaz2119@gmail.com>
2024-04-13 20:11:31 +0300

    update report file

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [
-    react(),
-    commonjs(), // Adding the commonjs plugin
-  ],
+  plugins: [react()],
 });
